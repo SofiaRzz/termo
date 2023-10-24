ed18a3d5196b1bee281cce815ac9f3c477222acc
SofiaRzz <sofiarizzottobrasil@gmail.com>
2023-10-24 10:21:09 -0300

    consumindo api

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {Table, TableWrapper, Row, Rows, Col, Cols, Cell} from 'react-native-table-component'
 
 export default function App() {
+  const [palavra, setPalavra] = useState(null);
+
+  async function fazerRequisicao() {
+    const resposta = await fetch("https://api.dicionario-aberto.net/random");
+    const json = await resposta.json();
+    const palavraSorteada = json.word
+    console.log(palavraSorteada)
+
+    if (palavraSorteada.length === 5) {
+      setPalavra(palavraSorteada)
+    } else {
+      fazerRequisicao()
+    }
+  }
+  useEffect(() => {fazerRequisicao()}, [])
+  //fazerRequisicao()
+    
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+        <Row>
+          <Col><TextInput style={styles.input}/></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Table>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}) 
+
