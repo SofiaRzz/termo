5da9497ef25206c8e6b8f841a986800e8197c542
SofiaRzz <sofiarizzottobrasil@gmail.com>
2023-10-31 01:52:47 -0300

    finalizado

--- App.jsx
+++ App.jsx
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect, useRef } from 'react';
-import { Alert, Pressable, StyleSheet, Text, View, TextInput, SafeAreaView, Button, Modal } from 'react-native';
+import { Alert, Pressable, StyleSheet, Text, View, TextInput, SafeAreaView, Button, Modal, TouchableOpacity } from 'react-native';
+import { useFonts } from 'expo-font';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'InterTight-Black': require('./assets/fonts/InterTight-Black.ttf'),
+    'InterTight-Bold': require('./assets/fonts/InterTight-Bold.ttf'),
+    'InterTight-ExtraBold': require('./assets/fonts/InterTight-ExtraBold.ttf'),
+    'InterTight-ExtraLight': require('./assets/fonts/InterTight-ExtraLight.ttf'),
+    'InterTight-Light': require('./assets/fonts/InterTight-Light.ttf'),
+    'InterTight-Medium': require('./assets/fonts/InterTight-Medium.ttf'),
+    'InterTight-Regular': require('./assets/fonts/InterTight-Regular.ttf'),
+    'InterTight-SemiBold': require('./assets/fonts/InterTight-SemiBold.ttf'),
+    'InterTight-Thin': require('./assets/fonts/InterTight-Thin.ttf'),
+});
+
   const [attempts, setAttempts] = useState(0);
   const inputs = [useRef(), useRef(), useRef(), useRef(), useRef()];
   const [inputValues, setInputValues] = useState(['', '', '', '', '']);
@@ -14,6 +28,7 @@ export default function App() {
   const [acertou, setAcertou] = useState(false);
   const [modalWinVisible, setModalWinVisible] = useState(false);
   const [modalDefeatVisible, setModalDefeatVisible] = useState(false);
+  const [modalExplicacaoVisible, setModalExplicacaoVisible] = useState(false);
   const [inputBackgroundColors, setInputBackgroundColors] = useState({
     0: '#FFFFFF',
     1: '#FFFFFF',
@@ -22,6 +37,7 @@ export default function App() {
     4: '#FFFFFF',
   });
 
+
   this.inputs = inputs;
 
 
@@ -29,8 +45,7 @@ export default function App() {
   async function fazerRequisicao() {
     const resposta = await fetch("https://api.dicionario-aberto.net/random");
     const json = await resposta.json();
-    const palavra = 'bolar';
-    // console.log(palavra);
+    const palavra = json.word;
 
     if (palavra.length === 5) {
       setPalavraSorteada(palavra);
@@ -61,6 +76,14 @@ export default function App() {
 
 
   }
+
+  const explicacao = () => {
+    setModalExplicacaoVisible(true);
+  };
+  
+  const closeModalExplicacao = () => {
+    setModalExplicacaoVisible(false);
+  };
 
   const handleInputChange = (text, index) => {
     const textUpper = text.toUpperCase();
@@ -99,11 +122,11 @@ export default function App() {
         const letraPS = splitPSorteada[index];
   
         if (letraPU === letraPS) {
-          letterColors[index] = '#00FF00'; // Verde em formato hexadecimal
+          letterColors[index] = '#8f9044'; // Verde em formato hexadecimal
         } else if (splitPSorteada.includes(letraPU)) {
-          letterColors[index] = '#FFFF00'; // Amarelo em formato hexadecimal
+          letterColors[index] = '#f8a523'; // Amarelo em formato hexadecimal
         } else {
-          letterColors[index] = '#808080'; // Cinza em formato hexadecimal
+          letterColors[index] = '#626869'; // Cinza em formato hexadecimal
         }
   
       });
@@ -114,7 +137,7 @@ export default function App() {
   
       setInputBackgroundColors(letterColors);
   
-      if (Object.values(letterColors).every((color) => color === '#00FF00')) {
+      if (Object.values(letterColors).every((color) => color === '#8f9044')) {
         setModalWinVisible(true)
         setAcertou(true);
       } 
@@ -148,8 +171,14 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <Text style={styles.titulo}>Termo</Text>
+        <View style={styles.duvidaContainer}>
+          <TouchableOpacity style={styles.btnDuvida} onPress={explicacao}>
+            <Text style={styles.interrogacao}>?</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.titulo}>TERMO</Text>
         <Text style={styles.subtitulo}>Você tem {6 - attempts} tentativas restantes</Text>
+
         {!acertou ? (views.map((view, viewIndex) => (
           <View key={viewIndex} style={styles.inputContainer}>
             {view.map((value, index) => (
@@ -165,7 +194,7 @@ export default function App() {
           </View>
         ))) : null}
         <View style={styles.inputContainer}>
-          {(inputValues.map((value, index) => (
+          {palavraSorteada ? (inputValues.map((value, index) => (
             <TextInput
               key={index}
               ref={this.inputs[index]}
@@ -177,7 +206,20 @@ export default function App() {
               value={value}
               maxLength={1}
             />
-          )))
+          ))) : (inputValues.map((value, index) => (
+            <TextInput
+              key={index}
+              editable={false}
+              ref={this.inputs[index]}
+              style={[styles.input, {
+                backgroundColor: acertou ? inputBackgroundColors[index] : 'white',
+              }]}
+              onChangeText={(text) => handleInputChange(text, index)}
+              onKeyPress={(event) => handleKeyPress(event, index)}
+              value={value}
+              maxLength={1}
+            />
+          ))) 
           }
         </View>
         <Modal
@@ -193,7 +235,7 @@ export default function App() {
                 onPress={() => restartGame()}
                 style={[styles.button, styles.buttonClose]}
               >
-                <Text style={styles.textStyle}>Reiniciar</Text>
+                <Text style={styles.textStyle}>REINICIAR</Text>
               </Pressable>
             </View>
           </View>
@@ -211,15 +253,37 @@ export default function App() {
                 onPress={() => restartGame()}
                 style={[styles.button, styles.buttonClose]}
               >
-                <Text style={styles.textStyle}>Reiniciar</Text>
+                <Text style={styles.textStyle}>REINICIAR</Text>
               </Pressable>
             </View>
           </View>
         </Modal>
-        <Button
-          title="Verificar"
-          onPress={btnVerificar}
-        />
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalExplicacaoVisible}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalExplicacao}>
+              <Text style={styles.modalText}>Seu objetivo é descobrir a palavra secreta em 6 tentativas</Text>
+              <Text style={styles.modalText}>Se uma letra está VERDE ela existe na palavra secreta e está no lugar correto</Text>
+              <Text style={styles.modalText}>Se uma letra está AMARELA ela existe na palavra secreta, mas em outro lugar</Text>
+              <Text style={styles.modalText}>Se uma letra está CINZA ela não existe na palavra secreta</Text>
+              <Text style={styles.modalText}>ATENÇÃO: Você deve acentuar palavras que tenham acento </Text>
+              
+              <Pressable
+                onPress={closeModalExplicacao}
+                style={[styles.button, styles.buttonClose]}
+              >
+                <Text style={styles.textStyle}>ENTENDI</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        <TouchableOpacity onPress={btnVerificar} style={styles.btnVerificar}>
+          <Text style={styles.textStyle}>VERIFICAR</Text>
+          </TouchableOpacity>
+
         <StatusBar style="auto" />
       </View>
 
@@ -230,22 +294,31 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#352f3d',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   titulo: {
-    fontSize: 20,
+    color: 'white',
+    fontSize: 30,
     marginBottom: 10,
+    fontFamily: 'InterTight-ExtraBold',
   },
+
   subtitulo: {
+    color: 'white',
     fontSize: 16,
     marginBottom: 10,
+    fontFamily: 'InterTight-Medium',
   },
+
   inputContainer: {
     flexDirection: 'row',
     marginBottom: 10,
   },
+
   input: {
     height: 50,
     width: 50,
@@ -255,10 +328,55 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     textAlign: 'center',
   },
+
   resultText: {
     fontSize: 16,
   },
 
+  duvidaContainer: {
+    position: 'absolute',
+    top: 50,
+    right: -30,
+  },
+
+  btnDuvida: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    borderRadius: 50,
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+
+  btnVerificar: {
+    backgroundColor: '#fc8020',
+    padding: 10,
+    borderRadius: 20,
+    
+  },
+
+  interrogacao: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: 'white',
+  },
+
+  modalExplicacao: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 50,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+
+  },
 
   centeredView: {
     flex: 1,
@@ -266,6 +384,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 22,
   },
+
   modalView: {
     margin: 20,
     backgroundColor: 'white',
@@ -281,23 +400,29 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
+
   button: {
     borderRadius: 20,
     padding: 10,
     elevation: 2,
   },
+
   buttonOpen: {
     backgroundColor: '#F194FF',
   },
+
   buttonClose: {
-    backgroundColor: '#2196F3',
+    backgroundColor: '#fc8020',
   },
+
   textStyle: {
     color: 'white',
-    fontWeight: 'bold',
+    fontFamily: 'InterTight-SemiBold',
     textAlign: 'center',
   },
+
   modalText: {
+    fontFamily: 'InterTight-Regular',
     marginBottom: 15,
     textAlign: 'center',
   },
